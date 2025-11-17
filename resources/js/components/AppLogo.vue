8d42833802e5b10667138e5aac5d1c6845b03c17
Daniel S. Ramos <danikzero@hotmail.com>
2025-11-17 13:51:40 -0300

    Alterando logo marca para a logo marca atual do sistema

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { Store } from 'lucide-vue-next';
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-xl 
-        bg-sidebar-primary text-sidebar-primary-foreground bg-gradient-to-r 
-        to-emerald-600 from-sky-400 shadow-md">
-        <!--<AppLogoIcon class="size-5 fill-current text-white dark:text-black" />-->
-        <component :is="Store" class="h-6 w-6" />
-    </div>
+    <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     <div class="text-left text-sm">
         <h2 class="font-extrabold leading-none">
             <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
