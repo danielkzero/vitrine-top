4c77160f7f0538be2cf9ea90b4a50566adb6cb65
Daniel Ramos <danikzero@hotmail.com>
2025-12-20 06:03:49 -0300

    Tentativa de alterar a linguagem de email enviado

---
+++ resources/lang/pt_BR/notifications.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'verify_email_subject' => 'Confirme seu endereço de e-mail',
+
+    'verify_email_greeting' => 'Olá!',
+
+    'verify_email_line1' => 'Por favor, clique no botão abaixo para verificar seu endereço de e-mail.',
+
+    'verify_email_action' => 'Verificar e-mail',
+
+    'verify_email_line2' => 'Se você não criou uma conta, nenhuma ação adicional é necessária.',
+
+    'verify_email_salutation' => 'Atenciosamente,',
+];
